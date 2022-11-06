651c64a413d94ea5d9e36fb77dc420480e60d968
Filipe Bacof <filipebacof@gmail.com>
2022-11-06 13:19:23 -0300

    placar adicionado

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function createGame(player1, hour, player2) {
+function createGame(player1, hour, player2, goal1, goal2) {
     return `
         <li>
-            <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+            <img src="./assets/icon-${player1}.svg" alt="Bandeira ${player1}" />
+            <div>${goal1 ? goal1 : ''}</div>
             <strong>${hour}</strong>
-            <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+            <div>${goal2 ? goal2 : ''}</div>
+            <img src="./assets/icon-${player2}.svg" alt="Bandeira ${player2}" />
         </li>
     `
 }
@@ -19,13 +21,13 @@ function createCard(date, day, games) {
     `
 }
 
-document.querySelector("#app").innerHTML=`
+document.querySelector("#app").innerHTML = `
     <header>
         <img src="./assets/logo.svg" alt="Logo da NLW" />
         <p>Copa Do Mundo</p>
     </header>
     <main id="cards">
-        ${createCard("20/11", "domingo", createGame('qatar', '13:00', 'ecuador'))}
+        ${createCard("20/11", "domingo", createGame('qatar', '13:00', 'ecuador', '0', '0'))}
         ${createCard("21/11", "segunda", createGame('england', '10:00', 'iran') + createGame('senegal', '13:00', 'netherlands') + createGame('united-states', '16:00', 'wales'))}
         ${createCard("22/11", "terça", createGame('argentina', '07:00', 'saudi-arabia') + createGame('denmark', '10:00', 'tunisia') + createGame('mexico', '13:00', 'poland') + createGame('france', '16:00', 'australia'))}
         ${createCard("23/11", "quarta", createGame('morocco', '07:00', 'croatia') + createGame('germany', '10:00', 'japan') + createGame('spain', '13:00', 'costa-rica') + createGame('belgium', '16:00', 'canada'))}
